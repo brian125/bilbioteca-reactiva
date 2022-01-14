ca7edd5ef80cc74e9892c2e388f2d3ba942b70d0
Brayan Silva <brayan_silva82151@elpoli.edu.co>
2022-01-14 10:01:38 -0500

    Buscar por area Router, caso de uso y test

---
+++ src/main/java/co/com/sofka/biblioteca/useCases/UseCaseBuscarPorArea.java
@@ -0,0 +1,27 @@
+package co.com.sofka.biblioteca.useCases;
+
+import co.com.sofka.biblioteca.dto.RecursoDTO;
+import co.com.sofka.biblioteca.mapper.RecursoMapper;
+import co.com.sofka.biblioteca.repository.RepositorioRecurso;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Flux;
+
+
+@Service
+@Validated
+public class UseCaseBuscarPorArea implements ObtenerPorString{
+
+    private final RepositorioRecurso repositorio;
+    private final RecursoMapper mapper;
+
+    public UseCaseBuscarPorArea(RepositorioRecurso repositorio, RecursoMapper mapper) {
+        this.repositorio = repositorio;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Flux<RecursoDTO> get(String area) {
+        return repositorio.findByArea(area).map(mapper.mapRecursoToDTO());
+    }
+}
