2d8150af83d08e516119e49da8252731ceffe4b1
Brayan Silva <brayan_silva82151@elpoli.edu.co>
2022-01-14 09:55:04 -0500

    Prestar, devolver y actualizar recurso router, caso de uso y test

--- src/main/java/co/com/sofka/biblioteca/repository/RepositorioRecurso.java
+++ src/main/java/co/com/sofka/biblioteca/repository/RepositorioRecurso.java
@@ -2,6 +2,8 @@ package co.com.sofka.biblioteca.repository;
 
 import co.com.sofka.biblioteca.collections.Recurso;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 public interface RepositorioRecurso extends ReactiveMongoRepository<Recurso, String> {
+    Flux<Recurso> findByArea(String area);
 }
---
+++ src/main/java/co/com/sofka/biblioteca/useCases/UseCasePrestarRecurso.java
@@ -0,0 +1,39 @@
+package co.com.sofka.biblioteca.useCases;
+
+import co.com.sofka.biblioteca.collections.Recurso;
+import co.com.sofka.biblioteca.dto.RecursoDTO;
+import co.com.sofka.biblioteca.mapper.RecursoMapper;
+import co.com.sofka.biblioteca.repository.RepositorioRecurso;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+
+@Service
+@Validated
+public class UseCasePrestarRecurso implements ObtenerDisponibilidad {
+
+    private final RepositorioRecurso repositorio;
+    private final RecursoMapper mapper;
+
+
+    public UseCasePrestarRecurso(RepositorioRecurso repositorio, RecursoMapper mapper) {
+        this.repositorio = repositorio;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Mono<String> get(String id) {
+        Mono<Recurso> recursoMono = repositorio.findById(id);
+        return recursoMono.flatMap(r -> {
+            if (r.isDisponible()) {
+                r.setDisponible(false);
+                r.setFecha(LocalDate.now());
+                repositorio.save(r);
+                return Mono.just("El recurso fue prestado con exito");
+            }
+            return Mono.just("El recurso no está disponible");
+        });
+    }
+}
